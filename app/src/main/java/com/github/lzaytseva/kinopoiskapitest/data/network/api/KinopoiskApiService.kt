ecd6200caff129b6f49cf283225f9a5aceac3afc
ledrabbit <lvzaytseva1@gmail.com>
2024-04-11 20:06:34 +0300

    added api service with get requests for: search with params, by title and movie details

---
+++ app/src/main/java/com/github/lzaytseva/kinopoiskapitest/data/network/api/KinopoiskApiService.kt
@@ -0,0 +1,24 @@
+package com.github.lzaytseva.kinopoiskapitest.data.network.api
+
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+import retrofit2.http.QueryMap
+
+interface KinopoiskApiService {
+
+    @GET("/v1.4/movie")
+    suspend fun searchMoviesByParams(
+        @QueryMap queryParams: Map<String, String>
+    )
+
+    @GET("/v1.4/movie/search")
+    suspend fun searchMoviesByTitle(
+        @Query("query") searchQuery: String,
+        @Query("page") page: Int,
+        @Query("limit") limit: Int
+    )
+
+    @GET("v1.4/movie/{id}")
+    suspend fun getMovieDetails(@Path("id") movieId: Int)
+}
