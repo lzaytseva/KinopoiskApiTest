c09f03a2e9c8f5ffbdb2e91387322cf1e98a84f4
ledrabbit <lvzaytseva1@gmail.com>
2024-04-11 19:50:28 +0300

    moved MainActivity to ui package

--- app/src/main/java/com/github/lzaytseva/kinopoiskapitest/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/github/lzaytseva/kinopoiskapitest/presentation/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.github.lzaytseva.kinopoiskapitest
+package com.github.lzaytseva.kinopoiskapitest.presentation.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.github.lzaytseva.kinopoiskapitest.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
