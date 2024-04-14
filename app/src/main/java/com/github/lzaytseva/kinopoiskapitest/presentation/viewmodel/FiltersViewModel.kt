5062e94e396d935366055f2ddd595af296b5a8e3
ledrabbit <lvzaytseva1@gmail.com>
2024-04-14 13:38:46 +0300

    added layout for filters, added fragment to nav graph

---
+++ app/src/main/java/com/github/lzaytseva/kinopoiskapitest/presentation/viewmodel/FiltersViewModel.kt
@@ -0,0 +1,9 @@
+package com.github.lzaytseva.kinopoiskapitest.presentation.viewmodel
+
+import com.github.lzaytseva.kinopoiskapitest.util.BaseViewModel
+import javax.inject.Inject
+
+class FiltersViewModel @Inject constructor(
+) : BaseViewModel() {
+
+}
